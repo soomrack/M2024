7d65ea2fc470634cbfc75832885a376e0a92af5d
Vladislav <47640760+liubvlad@users.noreply.github.com>
2024-02-13 13:17:00 +0300

    fix to snake_case

--- liubinetskii.vs/LiubTask1/LiubTask1/Entrace.c
+++ liubinetskii.vs/LiubTask1/LiubTask1/Entrace.c
@@ -10,149 +10,148 @@
 
 #pragma region Black box for calculating credit monthly payment
 
-double creditCalculationRecursion(double base, int exponent) {
+double сredit_сalculation_recursion(double base, int exponent) {
     if (exponent == 0)
         return 1;
     else
-        return base * creditCalculationRecursion(base, exponent - 1);
+        return base * сredit_сalculation_recursion(base, exponent - 1);
 }
 
-double getCreditMonthlyPayment(double creditAmount, double annualCreditRate, int durationMonths) {
-    double monthlyCreditRate = annualCreditRate / 12;
+double get_credit_monthly_payment(double credit_amount, double annual_credit_rate, int duration_months) {
+    double monthly_credit_rate = annual_credit_rate / 12;
 
-    double allCreditTimeInflation = creditCalculationRecursion((1 + monthlyCreditRate), durationMonths);
+    double all_credit_time_inflation = сredit_сalculation_recursion((1 + monthly_credit_rate), duration_months);
 
-    double coefficient = (monthlyCreditRate * allCreditTimeInflation) / (allCreditTimeInflation - 1);
+    double coefficient = (monthly_credit_rate * all_credit_time_inflation) / (all_credit_time_inflation - 1);
 
-    double monthlyPayment = creditAmount * coefficient;
+    double monthlyPayment = credit_amount * coefficient;
     return monthlyPayment;
 }
 
 #pragma endregion
 
 // Temporary shared variables 
-double creditAmount = 0;
-double creditRate = 0.18;
-double aliceCreditPayed = 0;
 
-double bobAccountAmount = 0;
-double accountRate = 0.16;
-
-double indexationRate = 0.12;
-
-double expensesRenovationYear = 100000;
-double expensesFoodCost = 15000;
-double aliceFlatCost = 20 * 1000 * 1000;
-double bobFlatRent = 25 * 1000;
+const int MONTHS_IN_YEAR = 12;
+const long long int INITIAL_MONEY = 2000000;
 
 
-double aliceSalary = 300000;
-double bobSalary = 200000;
+double credit_amount = 0;
+double credit_rate = 0.18;
+double alice_credit_payed = 0;
+
+double bob_account_amount = 0;
+double account_rate = 0.16;
+
+double indexation_rate = 0.12;
+
+double expenses_renovation_year = 100000;
+double expenses_food_cost = 15000;
+double alice_flat_cost = 20 * 1000 * 1000;
+double bob_flat_rent = 25 * 1000;
 
 
-void timeToAnnularPercents() {
+double alice_salary = 300000;
+double bob_salary = 200000;
+
+
+void time_to_annular_percents() {
     printf("--------timeToPercents--------\n");
 
-    double creditCharges = (creditAmount - aliceCreditPayed) * creditRate;
-    if (creditCharges > 0) {
-        creditAmount += creditCharges;
+    double credit_charges = (credit_amount - alice_credit_payed) * credit_rate;
+    if (credit_charges > 0) {
+        credit_amount += credit_charges;
     }
 
-    bobAccountAmount *= 1 + accountRate;
+    bob_account_amount *= 1 + account_rate;
 }
 
 
 // Годовая индексация
 double indexation(double amount) {
-    amount += amount * indexationRate;
+    amount += amount * indexation_rate;
     return amount;
 }
 
-void amountIndexation() {
-    expensesRenovationYear = indexation(expensesRenovationYear);
-    expensesFoodCost = indexation(expensesFoodCost);
-    aliceFlatCost = indexation(aliceFlatCost);
-    bobFlatRent = indexation(bobFlatRent);
+void amount_indexation() {
+    expenses_renovation_year = indexation(expenses_renovation_year);
+    expenses_food_cost = indexation(expenses_food_cost);
+    alice_flat_cost = indexation(alice_flat_cost);
+    bob_flat_rent = indexation(bob_flat_rent);
 
-    aliceSalary = indexation(aliceSalary);
-    bobSalary = indexation(bobSalary);
+    alice_salary = indexation(alice_salary);
+    bob_salary = indexation(bob_salary);
 }
 
-void printResult()
+void print_result()
 {
-    printf("Alice's capital: \n%.2lf rubs.\n", ((creditAmount - aliceCreditPayed) + aliceFlatCost));
-    printf("Bob's capital: \n%.2lf rubs.\n", bobAccountAmount);
+    printf("Alice's capital: \n%.2lf rubs.\n", ((credit_amount - alice_credit_payed) + alice_flat_cost));
+    printf("Bob's capital: \n%.2lf rubs.\n", bob_account_amount);
 }
 
 
 int main() {
-    const int MONTHS_IN_YEAR = 12;
-    const double INITIAL_MONEY = 2000000.0;
 
     // Alice & her mortgage
-    double mortgageAmount = 20000000.0;
-    double initialPayment = INITIAL_MONEY;
-    creditAmount = mortgageAmount - initialPayment;
+    double mortgage_amount = 20000000.0;
+    double initial_payment = INITIAL_MONEY;
+    credit_amount = mortgage_amount - initial_payment;
     
     //double aliceImpact = 300000;
 
-    int durationYears = 30;
-    int durationMonths = durationYears * MONTHS_IN_YEAR;
+    int duration_years = 30;
+    int duration_months = duration_years * MONTHS_IN_YEAR;
 
-    double mothlyPayment = 0;
+    double mothly_payment = 0;
 
-    mothlyPayment = getCreditMonthlyPayment(creditAmount, creditRate, durationMonths);
-	printf("Alice should pay every month = %.2f\n", mothlyPayment);
+    mothly_payment = getCreditMonthlyPayment(credit_amount, credit_rate, duration_months);
+	printf("Alice should pay every month = %.2f\n", mothly_payment);
     
 
     // Bob & his savings account 
-    bobAccountAmount = INITIAL_MONEY;
+    bob_account_amount = INITIAL_MONEY;
 
 
-    printf("Financial salary:\t Alice:%.2lf\t\t Bob:%.2lf\n", aliceSalary, bobSalary);
+    printf("Financial salary:\t Alice:%.2lf\t\t Bob:%.2lf\n", alice_salary, bob_salary);
 
 
     
-    for (int currentYear = 0; currentYear < durationYears; currentYear++) {
-        for (int monthInYear = 1; monthInYear <= MONTHS_IN_YEAR; monthInYear++)
+    for (int current_year = 0; current_year < duration_years; current_year++) {
+        for (int month_in_year = 1; month_in_year <= MONTHS_IN_YEAR; month_in_year++)
         {
-            int month = currentYear * MONTHS_IN_YEAR + monthInYear;
+            int month = current_year * MONTHS_IN_YEAR + month_in_year;
 
-            aliceCreditPayed += mothlyPayment;
-            bobAccountAmount += (bobSalary - expensesFoodCost - bobFlatRent);
+            alice_credit_payed += mothly_payment;
+            bob_account_amount += (bob_salary - expenses_food_cost - bob_flat_rent);
 
-            printf("Month='%d'\t alicePayed: %.2lf/%.2lf\t bobAccountAmount: %.2lf\n", month, aliceCreditPayed, creditAmount, bobAccountAmount);
+            printf("Month='%d'\t alice_payed: %.2lf/%.2lf\t bob_account_amount: %.2lf\n", month, alice_credit_payed, credit_amount, bob_account_amount);
 
         }
 
         // TEMP
-        if (currentYear == 29) break;
+        if (current_year == 29) break;
 
-        timeToAnnularPercents();
-        amountIndexation();
+        time_to_annular_percents();
+        amount_indexation();
 
 
     }
 
-    printResult();
+    print_result();
 
-    /*
-        // every month
-    //int monthsGoing = 20 * 12 + 1; // temp for debug
-
-    for (int month = 1; month <= monthsGoing; month++) {
-        
-        aliceCreditPayed += mothlyPayment;
-        bobAccountAmount += bobImpact;
-
-
-        printf("Month='%d'\t alicePayed: %.2lf/%.2lf\t bobAccountAmount: %.2lf\n", month, aliceCreditPayed, creditAmount, bobAccountAmount);
-
-        if (month % 12 == 0) {
-            timeToAnnularPercents();
-            printf("--------timeToPercents--------\n");
-        }
-    }
-    */
 	return 0;
 }
+
+
+
+/*
+Увеличить начальный капитал
+года текущие
+отсутпы 
+змея_кэйс
+
+в майн только важное
+
+Кредит работает как обязательство на n-лет
+
+*/
