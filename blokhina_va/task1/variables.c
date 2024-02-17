d9d49688c465d7e66306a192b47b4e4058ad3111
valentina <v.blohina@storageup.net>
2024-02-18 02:17:32 +0300

    Variables separated from main.c

---
+++ blokhina_va/task1/variables.c
@@ -0,0 +1,19 @@
+#include "./variables.h"
+
+// Alice
+float CREDIT = 18*1000*1000;
+float CREDIT_RATE = 0.11;
+
+// Bob
+float SALARY = 150*1000;
+
+// Common Expenses
+float WASTES = 30*1000;
+float FLAT_RENOVATION = 100*1000;
+int FLAT_RENT = 25*1000;
+float inflation = 0.04;
+int MONTHS = 30 * 12;
+int YEARS = 30;
+
+double a = 2.0f;
+int b = 3;
---
+++ blokhina_va/task1/variables.h
@@ -0,0 +1,18 @@
+// Alice
+extern float CREDIT;
+extern float CREDIT_RATE;
+
+// Bob
+extern float SALARY;
+
+// Common Expenses
+extern float WASTES;
+extern float FLAT_RENOVATION;
+extern int FLAT_RENT;
+extern float INFLATION;
+extern int MONTHS;
+extern int YEARS;
+
+
+extern double a;
+extern int b;
