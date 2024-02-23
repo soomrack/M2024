2bbc54854cb5a2cb63ffb0f7b6bdd68e70643d70
EgorYum <159125931+EgorYum@users.noreply.github.com>
2024-02-23 22:04:16 +0300

    Update Tack1.c

--- Yumalov_EA/Tack1.c
+++ Yumalov_EA/Tack1.c
@@ -1,40 +1,119 @@
 #include <stdio.h>
 #include <math.h>
-int zp =150000,eda=15000, gkx=10000,odezda=10000, kv = 30000,otdyx=50000;
-long B=2000000,A=0,kvartira=20000000;
-int num_of_years=30;// срок подсчета
-float monthly_payment () {
-    int annual_interest_rate=5;
-    // Преобразуем годовую процентную ставку в месячную
-    float monthly_interest_rate = annual_interest_rate / 12 / 100;
-    // Вычисляем общее количество платежей
-    int total_payments = num_of_years * 12;
-    // Вычисляем ежемесячный платеж используя формулу аннуитетного платежа
-   int ipoteka = ((kvartira-2000000) * monthly_interest_rate) / (1 - pow(1 + monthly_interest_rate, -total_payments));
-    return ipoteka;
-    
-}
-int main() {
-  int ipoteka = monthly_payment();
-  int rashod_a = eda+gkx+odezda+ipoteka;
-int rashod_b = eda+kv+odezda;
-for (int year = 2024 ;  year<2054; year++){//цикл-года
-       for (int month = 1; month<13; month++){//цикл-месяцы
-        B=B-rashod_b+zp;
-        A=A-rashod_a+zp;
-        B=B*1.01;
-    }
-    B=B-otdyx;
-    A=A-otdyx;
-  kvartira=kvartira*1.1;
- rashod_a=rashod_a*1.12;
-  rashod_b=rashod_b*1.12;
- zp=zp*1.12;
-}
-A=A+kvartira;
-printf("Alice=%d\n",A);
-printf("Bob=%d\n",B);
-printf("KVARTIRA=%d\n",kvartira);
 
-return 0;
+typedef long long Money;
+
+struct Person{
+    char* name;
+    Money salary;
+    Money bank_account;
+    Money credit_monthly_payment;
+    int saving_account_rate;
+    //int credit_rate;
+} alice, bob;
+
+//Alice
+Money kvartira = 20000000 * 100;
+Money kredit = 18000000 * 100;
+int ipoteka_rate = 5;
+Money remont = 50000 * 100;
+//Money wastes_alice = eda + odezda + intertaiments;
+
+//Bob
+Money rent = 25000 * 100;
+//Money vklad = 2000000 * 100;
+//int vklad_rate = 10;
+//Money wastes_bob = eda + odezda + intertaiments + rent;
+
+//Dla vsex
+Money eda = 15000 * 100;
+Money odezda = 5000 * 100; 
+Money intertaiments = 10000 * 100; 
+int inflation = 12;
+int num_of_years = 30;
+//Money salary = 150000 * 100;
+
+Money alice_monthly_payment () {
+    float monthly_ipoteka_rate = ipoteka_rate / 12 / 100;
+    int total_payments = num_of_years * 12; // Вычисляем общее количество платежей
+    Money ipoteka = (kredit * monthly_ipoteka_rate) / (1 - pow(1 + monthly_ipoteka_rate, -total_payments)); // Вычисляем ежемесячный платеж используя формулу аннуитетного платежа
+    return ipoteka;
 }
+
+long inflatia(long value, int time_unit){ // Функция считает сумму всех значений, повышенных на инфляцию
+    long sum = 0;
+    for ( int i = 0; i < time_unit; i++ ) {
+        value = value * (1 + inflation / 100);
+        sum = sum + value;
+    }
+    return sum;
+}
+  
+void init_bob () {
+    bob.salary = 150 * 1000 * 100;
+    bob.bank_account = 2 * 1000 * 1000 * 100;
+    bob.saving_account_rate = 16;
+}
+
+void init_alice () {
+    alice.salary = 150 * 1000 * 100;
+    alice.bank_account = 100 * 1000 * 100;
+    alice.credit_monthly_payment = alice_monthly_payment ();
+}
+
+int current_year = 2024;
+int current_month = 2;
+int end_year = 2024;
+int end_month = 2;
+
+void inflation_alice () {
+    if (current_month == 1) {
+        alice.salary *= (1.0 + inflation / 100.0);
+        kvartira *= (1.0 + inflation / 100.0);
+    }
+}
+void enrollment_alice () {
+    alice.bank_account += alice.salary;
+}
+
+void spendings_alice () {
+    alice.bank_account -= eda + odezda + intertaiments;
+    if (current_year < 2027) {
+        alice.bank_account -= remont;
+    }
+    alice.bank_account -= alice.credit_monthly_payment;
+}
+
+void print_alice () {
+    printf("Alice: money %lld\n", alice.bank_account + kvartira);
+}
+
+void processing () {
+    while (current_year * 12 + current_month != end_year * 12 + end_month) {
+        inflation_alice ();
+        enrollment_alice ();
+        spendings_alice ();
+        print_alice ();
+
+        current_month++;
+        if (current_month % 13 == 0) {
+            current_month = 1;
+            current_year++;
+        }
+    }
+
+}
+
+int main() {
+    init_alice ();
+    init_bob ();
+    processing ();
+   // inflatia (sum);
+   // print_results(alice_capital, bob_capital);
+    return 0;
+}
+// Функция для вывода результатов
+//void print_results(long alice_capital, long bob_capital) {
+ //   printf("Alice's capital including the value of the apartment in the end: %.2f rubles\n", alice_capital);
+ //   printf("Bob's capital in the end: %.2f rubles\n", bob_capital);
+ 
