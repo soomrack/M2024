ae9f50da3da1c29950ba9d3c03183bd74b7ebcc9
IvashkaNOW <144314269+IvashkaNOW@users.noreply.github.com>
2024-02-28 11:34:05 +0300

    task 1_ update

--- Kirov Ivan/Alice_bob.c
+++ Kirov Ivan/Alice_bob.c
@@ -18,6 +18,7 @@ struct Person {
     Money expenses;  // траты на еду, коммунальные платежи, одежду
     double mortgage_pp;
     Money mortgage_monthly_payment;
+    Money car;
 };
 
 struct Person bob;
@@ -31,9 +32,10 @@ void bob_init()
     bob.salary = 200 * 1000 * 100;
     bob.inflation_pp = 7;
     bob.house_price = 0;
-    strcpy(bob.name, "bob");
+    strcpy(bob.name, "Bob");
     bob.rent = 35 * 1000 * 100;
     bob.expenses = 10 * 1000 * 100;
+    
 }
 
 
@@ -44,11 +46,54 @@ void alice_init()
     alice.salary = 200 * 1000 * 100;
     alice.inflation_pp = 7;
     alice.house_price = 20 * 1000 * 1000 * 100;
-    strcpy(alice.name, "alice");
+    strcpy(alice.name, "Alice");
     alice.mortgage_first_fee = 1000 * 1000 * 100;
     alice.expenses = 10 * 1000 * 100;
     alice.mortgage_pp = 7;
     alice.mortgage_monthly_payment = 0;
+}
+void bob_car_buy (const int year, const int month) // покупка машины
+{
+    if(month==2 && year == 2026)
+    {
+        bob.bank_account = bob.bank_account - 2000*1000*100;
+    }
+    
+}
+
+void bob_car_tires (const int year, const int month)
+{
+    if (month ==4 && year >= 2026)
+    {
+        bob.expenses= bob.expenses+40*1000*100;
+    }
+    if (month ==5 && year >= 2026)
+    {
+        bob.expenses= bob.expenses-40*1000*100;
+    }
+    if (month ==10  && year >= 2026)
+    {
+        bob.expenses= bob.expenses+50*1000*100;
+    }
+    if (month ==11 && year >= 2026)
+    {
+        bob.expenses= bob.expenses-50*1000*100;
+    }
+    
+}
+
+
+void bob_car_to (const int year, const int month)
+{
+    if (month ==2 && year >= 2026)
+    {
+        bob.expenses= bob.expenses+15*1000*100;
+    }
+    if (month ==3 && year >= 2026)
+    {
+        bob.expenses= bob.expenses-15*1000*100;
+    }
+    
 }
 
 
@@ -137,11 +182,13 @@ void simulation()
 
 
     while (!(year == 2054 && month == 2)) {
-
+        bob_car_buy(year,month);
+        bob_car_to (year,month);
+        bob_car_tires(year,month);
         bob_deposite_income(year, month);
         bob_salary_income(year, month);
         bob_rent(year, month);
-        bob_expenses(year, month);  // траты на еду, коммунальные платежи, одежду
+        bob_expenses(year, month);  // траты на еду, коммунальные платежи, одежду,машину
 
         alice_deposite_income(year, month);
         alice_salary_income(year, month);
