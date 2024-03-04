69a18a0e6d2e296bec1746b8ab760f83487f47c5
IlyaGriff <ilyagriff11@gmail.com>
2024-03-04 20:58:50 +0300

    Update Task1.c
    some corrections

--- ivanov_i_o/Task1.c
+++ ivanov_i_o/Task1.c
@@ -31,14 +31,15 @@ void Alice_initiation()
     Alice.income = SALARY;
     Alice.capital = 0;
     Alice.life_spendings = LIFE_SPENDINGS;
-    Alice.monthly_spendings = (FLAT_PRICE - START_CAPITAL) * (BANK_RATE / 12.0) / (1 - powf((1 + BANK_RATE / 12.0), (1.0 - YEARS * 12.0)));
+    Alice.monthly_spendings = (FLAT_PRICE - START_CAPITAL) * (BANK_RATE / 12.0) / (1 - powf((1.0 + BANK_RATE / 12.0), (1.0 - YEARS * 12.0)));
     Alice.overpayment = Alice.monthly_spendings * 12 * YEARS - FLAT_PRICE;
 }
 
 
 void Alice_salary_income(int year, int month)
 {
-   if (month == 12) {
+   if (month == 12) 
+   {
        Alice.capital += (money)(Alice.income * 1.2);
    } 
    Alice.capital += Alice.income;
@@ -47,7 +48,8 @@ void Alice_salary_income(int year, int month)
 
 void Alice_deposite_income(int year, int month)
 {
-     if (year == 2034 && month == 1){
+     if (year == 2034 && month == 1)
+    {
         BANK_RATE = 0.1;
     }
     Alice.capital = Alice.capital * (1.0 + BANK_RATE / 12.0);
@@ -56,10 +58,11 @@ void Alice_deposite_income(int year, int month)
 
 void Alice_usual_spendings (int years, int month)
 {
-    if (month == 12) {
+    if (month == 12) 
+    {
         Alice.capital -= Alice.life_spendings * 1.5;
     }
-    Alice.capital = Alice.capital - Alice.life_spendings - Alice.monthly_spendings;
+    Alice.capital -= Alice.life_spendings - Alice.monthly_spendings;
 }
 
 
@@ -75,7 +78,8 @@ void Bob_initiation()
 
 void Bob_salary_income(int year, int month)
 {
-   if (month == 12) {
+   if (month == 12) 
+   {
        return;
    } 
    Bob.capital += Bob.income;
@@ -84,7 +88,8 @@ void Bob_salary_income(int year, int month)
 
 void Bob_deposite_income(int year, int month)
 {
-    if (year == 2034 && month == 1){
+    if (year == 2034 && month == 1)
+    {
         BANK_RATE = 0.1;
     }
    Bob.capital = Bob.capital * (1.0 + BANK_RATE / 12.0);
@@ -93,20 +98,23 @@ void Bob_deposite_income(int year, int month)
 
 void Bob_usual_spendings (int years, int month)
 {
-    if (month == 12) {
+    if (month == 12) 
+    {
         Bob.capital -= Bob.life_spendings * 1.5;
     }
-    Bob.capital = Bob.capital - Bob.life_spendings - Bob.monthly_spendings;
+    Bob.capital -=  Bob.life_spendings - Bob.monthly_spendings;
 }
 
 
 void inflation(int year, int month)
 {
-    if (month % 3 == 0 && month > 2) {
+    if (month % 3 == 0 && month > 2) 
+    {
         Alice.life_spendings += Alice.life_spendings * INFLATION / 4.0;
         Bob.life_spendings += Bob.life_spendings * INFLATION / 4.0;
     }
-    if (month == 12){
+    if (month == 12) 
+    {
         Alice.income = Alice.income * (1.0 + INFLATION / 2.0);
         Bob.income = Bob.income * (1.0 + INFLATION / 2.0);
         Bob.monthly_spendings = Bob.monthly_spendings * (1.0 + INFLATION / 2.0);
@@ -119,8 +127,8 @@ void simulation()
     int month = START_MONTH;
     int year = START_YEAR;
 
-    while(year != START_YEAR + YEARS && month != 1) {
-
+    while(year != START_YEAR + YEARS && month != 1) 
+    {
         Alice_deposite_income(year, month);
         Alice_salary_income(year, month);
         Alice_usual_spendings(year, month);
@@ -136,19 +144,24 @@ void simulation()
             year++;
             month -= 12;
         }
-
     }
 }
 
 
 void print_result()
 {
-    printf("Alice Capital: %lld\t",Alice.capital /= 100); printf("Alice Income: %lld\t", Alice.income /= 100); printf("Alice Life spendings: "
-    "%lld\t", Alice.life_spendings /= 100); printf("Alice Monthly payment: %lld\t", Alice.monthly_spendings /= 100); printf("Alice Overpayment: " 
-    "%lld\n", Alice.overpayment /= 100);
-    printf("Bob Capital: %lld\t", Bob.capital /= 100); printf("Bob Income: %lld\t", Bob.income /= 100); printf("Bob Life spendings: "
-    "%lld\t", Bob.life_spendings /= 100); printf("Bob Monthly payment: %lld\t", Bob.monthly_spendings /= 100); printf("Bob Overpayment: " 
-    "%lld\n", Bob.overpayment /= 100);
+    printf("Alice Capital: %lld\n",Alice.capital /= 100);
+    printf("Alice Income: %lld\n", Alice.income /= 100); 
+    printf("Alice Life spendings: %lld\n", Alice.life_spendings /= 100); 
+    printf("Alice Monthly spendings: %lld\n", Alice.monthly_spendings /= 100);
+    printf("Alice Overpayment: %lld\n", Alice.overpayment /= 100);
+
+    printf("Bob Capital: %lld\n", Bob.capital /= 100);
+    printf("Bob Income: %lld\n", Bob.income /= 100);
+    printf("Bob Life spendings: %lld\n", Bob.life_spendings /= 100);
+    printf("Bob Monthly spendings: %lld\n", Bob.monthly_spendings /= 100);
+    printf("Bob Overpayment: %lld\n", Bob.overpayment /= 100);
+
     printf("* all values presented above are in rubles");
 }
 
