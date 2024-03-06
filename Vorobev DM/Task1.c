8a1769e5ae2fd8dac46ff651d77eed7590cdb6c9
DmitryVorobev2511 <159234020+DmitryVorobev2511@users.noreply.github.com>
2024-03-06 11:09:37 +0300

    Add files via upload

---
+++ Vorobev DM/Task1.c
@@ -0,0 +1,360 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef long long int money;  // копейки
+
+int TOTAL_YEARS = 30;
+double INFLATION = 12 * 0.01;
+
+struct Person {
+    int if_has_apartment;
+    money apartment_price;
+    money savings;  // сумма денег на вкладе
+    int loan_period;  // в годах
+
+    // в мес€ц
+    money salary;
+    money food_spending;
+    money rent;
+    money loan_payment;  // вычисл€етс€ в функции compute_loan_payment
+    money additional_spendings;
+
+    // в год
+    money apartment_repairs;
+    double loan_rate;
+    double deposit_rate;
+};
+
+struct Person Bob;
+struct Person Alice;
+
+void calculation_loan_payment(struct Person*);  // вычисление ежемес€чной оплаты кредита
+
+void init_BOB();
+void BOBs_salary_increase(const int, const int);  // ежегодное повышение зарплаты
+void BOBs_salary_getting(const int, const int);  // ежемес€чное начисление зарплаты
+void BOB_buy_food(const int, const int);  // ежемес€чна€ покупка еды
+void BOB_pay_rent(const int, const int);  // ежемес€чна€ оплата аренды
+void BOB_pay_loan(const int, const int);  // ежемес€чна€ плата по кредиту
+void BOBs_unexpectable_spends(const int, const int);  // ежемес€чный учет дополнительных расходов
+void BOB_apartment_renovation(const int, const int);  // ежегодный ремонт квартиры
+void BOBs_percents_by_deposit(const int, const int);  //  ежемес€чна€ выплата процентов по вкладу
+void BOB_has_house();
+void BOB_print_results();  // ¬ывод результатов Ѕоба
+
+void init_ALICE();
+void ALICEs_salary_increase(const int, const int);  // ежегодное повышение зарплаты
+void ALICEs_salary_getting(const int, const int);  // ежемес€чное начисление зарплаты
+void ALICE_buy_food(const int, const int);  // ежемес€чна€ покупка еды
+void ALICE_pay_rent(const int, const int);  // ежемес€чна€ оплата аренды
+void ALICE_pay_loan(const int, const int);  // ежемес€чна€ плата по кредиту
+void ALICEs_unexpectable_spends(const int, const int);  // ежемес€чный учет дополнительных расходов
+void ALICE_apartment_renovation(const int, const int);  // ежегодный ремонт квартиры
+void ALICEs_percents_by_deposit(const int, const int);  //  ежемес€чна€ выплата процентов по вкладу
+void ALICE_has_house();
+void ALICE_print_results();  // ¬ывод результатов јлисы
+
+void inflate(const int, const int);  // ежемес€чный рост цен
+void calculation();  // вычисление результатов
+
+
+void calculation_loan_payment(struct Person* person)
+{
+    double numerator = pow(1 + person->loan_rate / 12, person->loan_period * 12);
+    double denominator = numerator - 1;
+    person->loan_payment = (money)((person->apartment_price - person->savings)
+        * person->loan_rate / 12 * numerator / denominator);
+
+    if (person->loan_rate == 0) {
+        person->loan_payment = 0;
+        person->if_has_apartment = 0;
+    }
+    else {
+        person->if_has_apartment = 1;
+    }
+
+    if (person->loan_payment > person->salary) {
+        printf("ѕрограмма остановлена: ѕлата по кредиту больше заработной платы");
+        exit(0);
+    }
+}
+
+
+void init_BOB()
+{
+    Bob.if_has_apartment = 0;
+    Bob.apartment_price = 20 * 1000 * 1000 * 100;
+    Bob.savings = 2 * 1000 * 1000 * 100;
+    Bob.loan_period = 0;
+    Bob.salary = 300 * 1000 * 100;
+    Bob.food_spending = 15000 * 100;
+    Bob.rent = 25000 * 100;
+    Bob.additional_spendings = 0;
+    Bob.apartment_repairs = 0;
+    Bob.loan_rate = 0;
+    Bob.deposit_rate = 16 * 0.01;
+
+    calculation_loan_payment(&Bob);
+}
+
+void BOBs_salary_increase(const int month, const int year)
+{
+    Bob.salary += Bob.salary * INFLATION;
+}
+
+
+void BOBs_salary_getting(const int month, const int year)
+{
+    Bob.savings += Bob.salary;
+
+    if (month == 12) {
+        BOBs_salary_increase(year, month);
+    }
+}
+
+
+void BOB_buy_food(const int month, const int year)
+{
+    Bob.savings -= Bob.food_spending;
+}
+
+
+void BOB_pay_rent(const int month, const int year)
+{
+    Bob.savings -= Bob.rent;
+}
+
+
+void BOB_pay_loan(const int month, const int year)
+{
+    Bob.savings -= Bob.loan_payment;
+}
+
+
+void BOBs_unexpectable_spends(const int month, const int year)
+{
+    Bob.savings -= Bob.additional_spendings;
+}
+
+
+void BOB_apartment_renovation(const int month, const int year)
+{
+    Bob.savings -= Bob.apartment_repairs;
+}
+
+
+void BOBs_percents_by_deposit(const int month, const int year)
+{
+    long double income = Bob.savings * Bob.deposit_rate / 12;
+    Bob.savings += income;
+}
+
+
+void BOB_has_house()
+{
+    if (!Bob.if_has_apartment) {
+        if (Bob.savings >= Bob.apartment_price) {
+            Bob.savings -= Bob.apartment_price;
+            Bob.if_has_apartment = 1;
+        }
+    }
+}
+
+
+void BOB_print_results()
+{
+    money Bob_savings_roubles = Bob.savings / 100;
+    long long int count_digits = 1;
+    while (count_digits < Bob_savings_roubles) {
+        count_digits *= 1000;
+    }
+    count_digits /= 1000;
+
+    printf("\nЌакоплени€ Ѕоба: ");
+
+    printf("%lld ", Bob_savings_roubles / count_digits);
+    Bob_savings_roubles %= count_digits;
+    count_digits /= 1000;
+
+    for (count_digits; count_digits > 0; count_digits /= 1000) {
+        int number_to_print = Bob_savings_roubles / count_digits;
+        printf("%03d ", number_to_print);
+        Bob_savings_roubles %= count_digits;
+    }
+    printf("рублей %02lld копеек", Bob.savings % 100);
+}
+void init_ALICE()
+{
+    Alice.if_has_apartment = 0;
+    Alice.apartment_price = 20 * 1000 * 1000 * 100;
+    Alice.savings = 2 * 1000 * 1000 * 100;
+    Alice.loan_period = 30;
+    Alice.salary = 300 * 1000 * 100;
+    Alice.food_spending = 15000 * 100;
+    Alice.rent = 0;
+    Alice.additional_spendings = 0;
+    Alice.apartment_repairs = 100 * 1000 * 100;
+    Alice.loan_rate = 18 * 0.01;
+    Alice.deposit_rate = 16 * 0.01;
+
+    calculation_loan_payment(&Alice);
+}
+
+
+void ALICEs_salary_increase(const int month, const int year)
+{
+    Alice.salary += Alice.salary * INFLATION;
+}
+
+
+void ALICEs_salary_getting(const int month, const int year)
+{
+    Alice.savings += Alice.salary;
+
+    if (month == 12) {
+        ALICEs_salary_increase(year, month);
+    }
+}
+
+void ALICE_buy_food(const int month, const int year)
+{
+    Alice.savings -= Alice.food_spending;
+}
+
+
+void ALICE_pay_rent(const int month, const int year)
+{
+    Alice.savings -= Alice.rent;
+}
+
+void ALICE_pay_loan(const int month, const int year)
+{
+    Alice.savings -= Alice.loan_payment;
+}
+
+
+void ALICEs_unexpectable_spends(const int month, const int year)
+{
+    Alice.savings -= Alice.additional_spendings;
+}
+
+void ALICE_apartment_renovation(const int month, const int year)
+{
+    Alice.savings -= Alice.apartment_repairs;
+}
+
+void ALICEs_percents_by_deposit(const int month, const int year)
+{
+    long double income = Alice.savings * Alice.deposit_rate / 12;
+    Alice.savings += income;
+}
+
+
+void ALICE_has_house()
+{
+    if (!Alice.if_has_apartment) {
+        if (Alice.savings >= Alice.apartment_price) {
+            Alice.savings -= Alice.apartment_price;
+            Alice.if_has_apartment = 1;
+        }
+    }
+}
+
+void ALICE_print_results()
+{
+    money Alice_savings_roubles = Alice.savings / 100;
+    long long int count_digits = 1;
+    while (count_digits < Alice_savings_roubles) {
+        count_digits *= 1000;
+    }
+    count_digits /= 1000;
+
+    printf("\nЌакоплени€ јлисы: ");
+
+    printf("%lld ", Alice_savings_roubles / count_digits);
+    Alice_savings_roubles %= count_digits;
+    count_digits /= 1000;
+
+    for (count_digits; count_digits > 0; count_digits /= 1000) {
+        int number_to_print = Alice_savings_roubles / count_digits;
+        printf("%03d ", number_to_print);
+        Alice_savings_roubles %= count_digits;
+    }
+    printf("рублей %02lld копеек", Alice.savings % 100);
+}
+
+
+
+void inflate(const int month, const int year)
+{
+    Bob.food_spending += Bob.food_spending * INFLATION / 12;
+    Bob.additional_spendings += Bob.additional_spendings * INFLATION / 12;
+
+    Alice.food_spending += Alice.food_spending * INFLATION / 12;
+    Alice.additional_spendings += Alice.additional_spendings * INFLATION / 12;
+}
+
+
+
+void calculation()
+{
+    time_t time_ = time(NULL);
+    struct tm time_now = *localtime(&time_);
+
+    int starting_year = time_now.tm_year + 1900;
+    int starting_month = time_now.tm_mon + 1;
+    int year = starting_year;
+    int month = starting_month;
+
+    while (!(year == starting_year + TOTAL_YEARS && month == starting_month)) {
+        BOBs_salary_getting(year, month);
+        BOB_buy_food(year, month);
+        BOB_pay_rent(year, month);
+        BOB_pay_loan(year, month);
+        BOBs_unexpectable_spends(year, month);
+        BOBs_percents_by_deposit(year, month);
+        BOB_has_house();
+
+        ALICEs_salary_getting(year, month);
+        ALICE_buy_food(year, month);
+        ALICE_pay_rent(year, month);
+        ALICE_pay_loan(year, month);
+        ALICEs_unexpectable_spends(year, month);
+        ALICEs_percents_by_deposit(year, month);
+        ALICE_has_house();
+
+        inflate(year, month);
+
+        if (month == 12) {
+            BOB_apartment_renovation(year, month);
+            ALICE_apartment_renovation(year, month);
+        }
+
+        if (month != 12) {
+            month += 1;
+        }
+        else {
+            month = 1;
+            year += 1;
+        }
+
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    init_BOB();
+    init_ALICE();
+
+    calculation();
+
+    BOB_print_results();
+    ALICE_print_results();
+}
