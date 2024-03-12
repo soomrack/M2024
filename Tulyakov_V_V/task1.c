adb768255ba6ec92a6ba7e42f94d7ef5221bf87b
Вячеслав Туляков <159118043+VyachTulyakov@users.noreply.github.com>
2024-03-12 12:40:47 +0300

    Update task1.c

--- Tulyakov_V_V/task1.c
+++ Tulyakov_V_V/task1.c
@@ -1,112 +1,240 @@
 #include <stdio.h>
 #include <math.h>
+#include <locale.h>
+#include <stdlib.h>
 
-struct apartment_data {
-    int price; // Стоимость квартиры
-    int renovation_years; // Годы на ремонт
-    int renovation_cost_per_year; // Расходы на ремонт в год
-    int mortgage_years; // Срок ипотеки в годах
+
+typedef long long int Money; // рубли
+double INFLIATION = 12 * 0.01;
+double DEPOSIT_RATE = 16 * 0.01;
+double MORTGAGE_PERCENTAGE = 18 * 0.01;
+int LOAN_YEARS = 30;
+Money ALICE_PAYMENT;
+
+struct Person
+{
+    Money salary;
+    Money food_spending;
+    Money home_price;
+    Money rent;
+    Money additional_spendings;
+    Money savings;
+    int loan_years;
+    Money contribution;
+    Money appartment_repair;
+    Money deposit;
 };
 
-struct finance_data {
-    int initial_capital; // Начальный капитал 
-    int mortgage_rate; // Процентная ставка по ипотеке
-    int savings_interest_rate; // Процентная ставка по сбережениям
-    int inflation_rate; // Инфляция 
-    int monthly_salary; // Ежемесячная зарплата 
-    int monthly_expenses; // Расходы на еду в месяц 
-    int vacation_expenses; // Расходы на отпуск в год 
+
+struct Person Alice
+{
+    Alice.salary = 250 * 1000,
+        Alice.food_spending = 20 * 1000,
+        Alice.home_price = 20 * 1000 * 1000,
+        Alice.rent = 0,
+        Alice.additional_spendings = 30 * 1000,
+        Alice.savings = 2 * 1000 * 1000,
+        Alice.loan_years = 30,
+        Alice.contribution = 2 * 1000 * 1000,
+        Alice.appartment_repair = 150 * 1000,
+        Alice.deposit = 0,
 };
 
-struct apartment_data initialize_apartment_data() {
-    struct apartment_data data;
-    data.price = 20000000; 
-    data.renovation_years = 2;
-    data.renovation_cost_per_year = 1000000; 
-    data.mortgage_years = 30;
-    return data;
+
+struct Person Bob
+{
+    Bob.salary = 300 * 1000,
+        Bob.food_spending = 20 * 1000,
+        Bob.home_price = 0,
+        Bob.rent = 30 * 1000,
+        Bob.additional_spendings = 50 * 1000,
+        Bob.savings = 2 * 1000 * 1000,
+        Alice.loan_years = 30,
+        Bob.contribution = 0,
+        Bob.appartment_repair = 0,
+        Bob.deposit = 0,
+};
+
+
+void Alice_indexation()
+{
+    Alice.salary += Alice.salary * INFLIATION;
+    Alice.food_spending += Alice.food_spending * INFLIATION;
+    Alice.home_price += Alice.home_price * INFLIATION;
+    Alice.additional_spendings += Alice.additional_spendings * INFLIATION;
 }
 
-struct finance_data initialize_finance_data() {
-    struct finance_data data;
-    data.initial_capital = 2000000; 
-    data.mortgage_rate = 16;
-    data.savings_interest_rate = 12; 
-    data.inflation_rate = 11; 
-    data.monthly_salary = 360000;
-    data.monthly_expenses = 40000; 
-    data.vacation_expenses = 200000;
-    return data;
+
+void Alice_mortgage() // расчет ипотеки
+{
+    double monthly_rate = MORTGAGE_PERCENTAGE / 12;
+    double total_rate = pow(1 + monthly_rate, LOAN_YEARS * 12);
+    ALICE_PAYMENT = (Alice.home_price - Alice.savings) * monthly_rate * total_rate / (total_rate - 1);
 }
 
-// Функция для расчета ежемесячного платежа по ипотеке
-int calculate_monthly_mortgage_payment(struct apartment_data data, int mortgage_rate) {
-    double mortgage_rate_monthly = (double)mortgage_rate / (12 * 100);
-    int total_months = data.mortgage_years * 12;
-    int monthly_mortgage_payment = (int)(data.price * mortgage_rate_monthly /
-        (1 - pow(1 + mortgage_rate_monthly, -total_months)));
-    return monthly_mortgage_payment;
+
+void Alice_get_salary()
+{
+    Alice.savings += Alice.salary;
 }
 
-// Функция для моделирования финансовых операций на протяжении срока ипотеки
-void simulate_financial_operations(struct apartment_data apartment_data, struct finance_data finance_data, int monthly_mortgage_payment, int* alice_final_capital, int* bob_final_capital) {
-    int alice_mortgage = apartment_data.price - finance_data.initial_capital;
-    int total_alice_payments = monthly_mortgage_payment * apartment_data.mortgage_years * 12;
-    int total_alice_earnings = 0;
-    int bob_savings = finance_data.initial_capital;
-    int total_bob_savings_with_interest = bob_savings;
-    int total_bob_payments = 0;
-    int total_bob_earnings = 0;
 
-    // Вычисление расходов и доходов за весь срок ипотеки
-    for (int year = 1; year <= apartment_data.mortgage_years; ++year) {
-        apartment_data.price *= (10000 + finance_data.inflation_rate) / 10000;
-        finance_data.mortgage_rate *= (10000 + finance_data.inflation_rate) / 10000.0;
-        int renovation_cost = (year <= apartment_data.renovation_years) ? apartment_data.renovation_cost_per_year : 0;
-        int monthly_expenses_adjusted = finance_data.monthly_expenses * 12 * year *
-            (10000 + finance_data.inflation_rate) / 10000;
-        finance_data.monthly_salary *= (10000 + finance_data.inflation_rate) / 10000.0;
-        int bob_income = finance_data.monthly_salary * 12;
-        total_bob_earnings += bob_income;
-        bob_savings += bob_income - monthly_expenses_adjusted - renovation_cost - finance_data.vacation_expenses;
-        total_bob_savings_with_interest *= (10000 + finance_data.savings_interest_rate) / 10000.0;
-        total_bob_savings_with_interest += bob_savings;
-        total_bob_payments += monthly_mortgage_payment;
-        total_alice_earnings += finance_data.monthly_salary * 12;
-    }
-    // Вычисление суммы денег, которую Alice внесла по ипотеке
-    int alice_mortgage_contribution = total_alice_payments - alice_mortgage;
-
-    // Вычисление капитала Alice с учетом стоимости квартиры в итоге
-    *alice_final_capital = total_alice_earnings - total_alice_payments + apartment_data.price;
-
-    // Вычисление капитала Bob с учетом стоимости квартиры в итоге
-    *bob_final_capital = total_bob_savings_with_interest - total_bob_payments + apartment_data.price;
+void Alice_get_food()
+{
+    Alice.savings -= Alice.food_spending;
 }
 
-// Функция для вывода результатов
-void print_results(int alice_final_capital, int bob_final_capital) {
-    printf("Alice's capital including the value of the apartment in the end: %d rubles\n", alice_final_capital);
-    printf("Bob's capital including the value of the apartment in the end: %d rubles\n", bob_final_capital);
 
-    // Определение победителя
-    if (alice_final_capital > bob_final_capital) {
-        printf("Alice remains in greater advantage\n");
-    }
-    else if (alice_final_capital < bob_final_capital) {
-        printf("Bob remains in greater advantage\n");
-    }
-    else {
-        printf("Both Alice and Bob remain in equal advantage\n");
+void Alice_buy_home()
+{
+    Alice.savings -= Alice.contribution;
+}
+
+
+void Alice_mortgage_payment()
+{
+    Alice.savings -= ALICE_PAYMENT;
+}
+
+
+void Alice_additional_spendings()
+{
+    Alice.savings -= Alice.additional_spendings;
+}
+
+
+void Alice_appartment_repair(int year, int month)
+{
+    if (year == 2024 && (month == 3 || month == 4))
+        Alice.savings -= Alice.appartment_repair;
+}
+
+
+void Alice_deposit()
+{
+    if (Alice.savings >= (ALICE_PAYMENT * 2)) {
+        int Alice_deposit_profit;
+        Alice.savings -= ALICE_PAYMENT;
+        Alice.deposit += ALICE_PAYMENT;
+        Alice_deposit_profit = Alice.deposit * DEPOSIT_RATE / 365 * 30;
+        Alice.deposit += Alice_deposit_profit;
     }
 }
 
-int main() {
-    struct apartment_data apartment_data = initialize_apartment_data();
-    struct finance_data finance_data = initialize_finance_data();
-    int monthly_mortgage_payment = calculate_monthly_mortgage_payment(apartment_data, finance_data.mortgage_rate);
-    int alice_final_capital, bob_final_capital;
-    simulate_financial_operations(apartment_data, finance_data, monthly_mortgage_payment, &alice_final_capital, &bob_final_capital);
-    print_results(alice_final_capital, bob_final_capital);
-    return 0;
+
+void Alice_savings()
+{
+    Alice.savings += Alice.home_price;
+    Alice.savings += Alice.deposit;
+}
+
+
+void Bob_indexation()
+{
+    Bob.salary += Bob.salary * INFLIATION;
+    Bob.food_spending += Bob.food_spending * INFLIATION;
+    Bob.rent += Bob.rent * INFLIATION;
+}
+
+
+void Bob_get_salary()
+{
+    Bob.savings += Bob.salary;
+}
+
+
+void Bob_get_food()
+{
+    Bob.savings -= Bob.food_spending;
+}
+
+
+void Bob_additional_spendings()
+{
+    Bob.savings -= Bob.additional_spendings;
+}
+
+
+void Bob_deposit()
+{
+    int Bob_deposit_profit;
+    Bob.deposit += (Bob.salary - Bob.home_price - Bob.additional_spendings - Bob.rent);
+    Bob_deposit_profit = Bob.deposit * DEPOSIT_RATE / 365 * 30;
+    Bob.deposit += Bob_deposit_profit;
+}
+
+
+void Bob_savings()
+{
+    Bob.savings += Bob.deposit;
+}
+
+
+void Alice_simulation()
+{
+    int month = 3;
+    int year = 2024;
+    Alice_mortgage();
+    Alice_buy_home();
+    while (year <= (year + LOAN_YEARS))
+    {
+        Alice_get_salary();
+        Alice_get_food();
+        Alice_mortgage_payment();
+        Alice_appartment_repair(year, month);
+        Alice_deposit();
+
+        if (year == 2024 + LOAN_YEARS && month == 3)
+            break;
+
+        month++;
+
+        if (month == 13) {
+            month = 1;
+            year++;
+            Alice_indexation();
+        }
+    }
+    Alice_savings();
+}
+
+
+void Bob_simulation()
+{
+    int month = 3;
+    int year = 2024;
+    while (year <= (year + LOAN_YEARS))
+    {
+        Bob_get_salary();
+        Bob_get_food();
+        Bob_deposit();
+
+        if (year == 2024 + LOAN_YEARS && month == 3)
+            break;
+
+        month++;
+
+        if (month == 13) {
+            month = 1;
+            year++;
+            Bob_indexation();
+        }
+    }
+    Bob_savings();
+}
+
+
+int main()
+{
+    Alice_simulation();
+    Bob_simulation();
+    setlocale(LC_ALL, "Rus");
+    printf("Сбережения Элис: %lld руб\nСбережения Боба: %lld руб\n", Alice.savings, Bob.savings);
+    if (Alice.savings > Bob.savings)
+        printf("Элис в большей выгоде.");
+    else if (Alice.savings < Bob.savings)
+        printf("Боб в большей выгоде.");
+    else
+        printf("Элис и Боб остались с одинаковым количеством капитала.");
+    return 1;
 }
