17d82dfd7167f074b712c4213f34b83271bdee54
Obshatko Alexey <79208718+oxton12@users.noreply.github.com>
2024-03-22 19:35:38 +0300

    Update task3.cpp

--- Obshatko AI/task3.cpp
+++ Obshatko AI/task3.cpp
@@ -4,90 +4,124 @@
 #include <math.h>
 #include <locale.h>
 
-size_t counter = 0;
+
+class matrix_size_error: public std::exception
+{
+public:
+    matrix_size_error(const std::string& message): message{message}
+    {}
+    const char* what() const noexcept override
+    {
+        return message.c_str();
+    }
+private:
+    std::string message;
+};
+
+
+class matrix_data_error: public std::exception
+{
+public:
+    matrix_data_error(const std::string& message): message{message}
+    {}
+    const char* what() const noexcept override
+    {
+        return message.c_str();
+    }
+private:
+    std::string message;
+};
+
 
 class Matrix {
-    private:
+private:
     size_t cols;
     size_t rows;
     double* data = nullptr;
-    size_t id;
+    size_t id;  // для разработчика
+    static int counter;  // для разработчика
 
-    public:
-    Matrix()
-    {
-        id = ++counter;
-        std::cout << "вызван конструктор нулевой матрицы с id " << id << std::endl;
-        cols = 0;
-        rows = 0;
-    }
-
-
-    Matrix(const size_t received_cols, const size_t received_rows)
-    {
-        id = ++counter;
-        std::cout << "вызван конструктор матрицы с id " << id << std::endl;
-        if(SIZE_MAX / received_cols < received_rows) {
-            //matrix_log(ERROR, "Матрица слишком большая");
-            cols = 0;
-            rows = 0;
-            data = NULL;
-            return;
-        }
-
-        cols = received_cols;
-        rows = received_rows;
-        if(cols * rows != 0) {
-            data = new double[cols * rows];
-        } else {
-            data = nullptr;
-        }
-    }
-
-
-    Matrix(const Matrix &matrix)
-    {
-        id = ++counter;
-        std::cout << "вызван конструктор копирования для матрицы с id " << id << std::endl;
-        cols = matrix.cols;
-        rows = matrix.rows;
-        if(cols * rows != 0) {
-            data = new double[cols * rows];
-            memcpy(data, matrix.data, sizeof(double) * matrix.cols * matrix.rows);
-        }
-    }
-
-
-    Matrix(Matrix &&matrix)  // Matrix A(C+B);
-    {
-        id = ++counter;
-        std::cout << "вызван конструктор переноса для матрицы с id " << id << std::endl;
-        cols = matrix.cols;
-        rows = matrix.rows;
-        data = matrix.data;
-        matrix.data = nullptr;
-    }
-
-
-    ~Matrix()
-    {
-        std::cout << "вызван деструктор для матрицы с id " << id << std::endl;
-        delete data;
-    }
-
+public:
+    Matrix();
+    Matrix(const size_t received_cols, const size_t received_rows);
+    Matrix(Matrix &matrix);
+    Matrix(Matrix &&matrix);
+    ~Matrix();
 
     void fill_random(size_t);
     void fill_zeros();
     void unit();
+    void matrix_row_add(size_t result_row, size_t adding_row, double multiplyer);
+    void matrix_row_substract(size_t result_row, size_t adding_row, double multiplyer);
     Matrix& operator=(const Matrix&);
     Matrix& operator=(Matrix&&);
-    Matrix& operator+(const Matrix&);
-    Matrix& operator-(const Matrix&);
-    Matrix& operator*(const Matrix&);
-    Matrix& operator*(const double);
-    Matrix& exponential(unsigned int);
+    Matrix operator+(const Matrix&);
+    Matrix operator-(const Matrix&);
+    Matrix operator*(const Matrix&);
+    Matrix operator*(const double);
+    Matrix exponential(unsigned int);
+    double det(double accuracy);
     friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
 };
+int Matrix::counter = 0;  // для разработчика
+
+
+Matrix::Matrix() 
+{
+    id = ++counter;  // для разработчика
+    std::cout << "вызван конструктор нулевой матрицы с id " << id << std::endl;  // для разработчика
+    cols = 0;
+    rows = 0;
+}
+
+
+Matrix::Matrix(const size_t received_cols, const size_t received_rows) : cols {received_cols}, rows {received_rows}
+{
+    id = ++counter;  // для разработчика
+    std::cout << "вызван конструктор матрицы с id " << id << std::endl;  // для разработчика
+    data = nullptr;
+    if(SIZE_MAX / received_cols < received_rows) {
+        throw matrix_size_error("Матрица слишком большая");
+        cols = 0;
+        rows = 0;
+        return;
+    }
+    
+    if(cols != 0 && rows != 0) {
+        data = new double[cols * rows];
+    }
+}
+
+
+Matrix::Matrix(Matrix &matrix)
+{
+    id = ++counter;  // для разработчика
+    std::cout << "вызван конструктор копирования для матрицы с id " << id << std::endl;  // для разработчика
+    cols = matrix.cols;
+    rows = matrix.rows;
+    if(cols != 0 && rows != 0) {
+        data = new double[cols * rows];
+        memcpy(data, matrix.data, sizeof(double) * matrix.cols * matrix.rows);
+    }
+}
+
+
+Matrix::Matrix(Matrix &&matrix)  // Matrix A(C+B);
+{
+    id = ++counter;  // для разработчика
+    std::cout << "вызван конструктор переноса для матрицы с id " << id << std::endl;  // для разработчика
+    cols = matrix.cols;
+    rows = matrix.rows;
+    data = matrix.data;
+    matrix.data = nullptr;
+}
+
+
+Matrix::~Matrix()
+{
+    std::cout << "вызван деструктор для матрицы с id " << id << std::endl;  // для разработчика
+    delete data;
+}
 
 
 void Matrix::fill_random(size_t max_value)
@@ -116,12 +150,28 @@ void Matrix::unit()
 }
 
 
+void Matrix::matrix_row_add(size_t result_row, size_t adding_row, double multiplyer)
+{
+    for(int col = 0; col < cols; col++) {
+        data[result_row * cols + col] += data[adding_row * cols + col] * multiplyer;
+    }
+}
+
+
+void Matrix::matrix_row_substract(size_t result_row, size_t adding_row, double multiplyer)
+{
+    for(int col = 0; col < cols; col++) {
+        data[result_row * cols + col] -= data[adding_row * cols + col] * multiplyer;
+    }
+}
+
+
 Matrix& Matrix::operator=(const Matrix& matrix) {
-    std::cout << "вызван оператор присваивания с копированием для матрицы с id " << id << std::endl;
+    std::cout << "вызван оператор присваивания с копированием для матрицы с id " << id << std::endl;  // для разработчика
     if(data != matrix.data) {
         cols = matrix.cols;
         rows = matrix.rows;
-        if(cols * rows != 0) {
+        if(cols != 0 && rows != 0) {
             delete data;
             memcpy(data, matrix.data, sizeof(double) * matrix.cols * matrix.rows);
         }
@@ -131,7 +181,7 @@ Matrix& Matrix::operator=(const Matrix& matrix) {
 
 
 Matrix& Matrix::operator=(Matrix&& matrix) {
-    std::cout << "вызван оператор присваивания с переносом для матрицы с id " << id << std::endl;
+    std::cout << "вызван оператор присваивания с переносом для матрицы с id " << id << std::endl;  // для разработчика
     cols = matrix.cols;
     rows = matrix.rows;
     delete data;
@@ -141,46 +191,46 @@ Matrix& Matrix::operator=(Matrix&& matrix) {
 }
 
 
-Matrix& Matrix::operator+(const Matrix& matrix_to_add) {
-    std::cout << "вызван оператор сложения" << std::endl;
+Matrix Matrix::operator+(const Matrix& matrix_to_add) {
+    std::cout << "вызван оператор сложения" << std::endl;  // для разработчика
     // проверка равности размерноти матриц
     if(cols != matrix_to_add.cols || rows != matrix_to_add.rows) {
-        throw std::invalid_argument("Для выполнения операции сложения матрицы должны иметь одинаковые размерности");
+        throw matrix_size_error("Для выполнения операции сложения матрицы должны иметь одинаковые размерности");
     }
 
-    Matrix *result_matrix = new Matrix(cols, rows);
+    Matrix result_matrix = Matrix(cols, rows);
 
     for(size_t index = 0; index < rows * cols; index++) {
-        result_matrix->data[index] = data[index] + matrix_to_add.data[index];
+        result_matrix.data[index] = data[index] + matrix_to_add.data[index];
     }
-    return *result_matrix;
+    return result_matrix;
 }
 
 
-Matrix& Matrix::operator-(const Matrix& matrix_to_add) {
-    std::cout << "вызван оператор вычитания" << std::endl;
+Matrix Matrix::operator-(const Matrix& matrix_to_add) {
+    std::cout << "вызван оператор вычитания" << std::endl;  // для разработчика
     // проверка равности размерноти матриц
     if(cols != matrix_to_add.cols || rows != matrix_to_add.rows) {
-        throw std::invalid_argument("Для выполнения операции вычитания матрицы должны иметь одинаковые размерности");
+        throw matrix_size_error("Для выполнения операции вычитания матрицы должны иметь одинаковые размерности");
     }
 
-    Matrix *result_matrix = new Matrix(cols, rows);
+    Matrix result_matrix = Matrix(cols, rows);
 
     for(size_t index = 0; index < rows * cols; index++) {
-        result_matrix->data[index] = data[index] - matrix_to_add.data[index];
+        result_matrix.data[index] = data[index] - matrix_to_add.data[index];
     }
-    return *result_matrix;
+    return result_matrix;
 }
 
 
-Matrix& Matrix::operator*(const Matrix& matrix_to_multiply) {
-    std::cout << "вызван оператор умножения" << std::endl;
+Matrix Matrix::operator*(const Matrix& matrix_to_multiply) {
+    std::cout << "вызван оператор умножения" << std::endl;  // для разработчика
     // проверка равности размерноти матриц
     if(cols != matrix_to_multiply.rows) {
-        throw std::invalid_argument("Для выполнения операции умножения матрицы должны иметь согласованные размерности");
+        throw matrix_size_error("Для выполнения операции умножения матрицы должны иметь согласованные размерности");
     }
 
-    Matrix *result_matrix = new Matrix(matrix_to_multiply.cols, rows);
+    Matrix result_matrix = Matrix(matrix_to_multiply.cols, rows);
 
     size_t cols_of_A = cols;  // количество столбцов первой матрицы
     size_t operations_per_element = cols_of_A;  // количество операций умножения на каждый элемент результирующей матрицы
@@ -194,45 +244,94 @@ Matrix& Matrix::operator*(const Matrix& matrix_to_multiply) {
                 current_element += A_element * B_element;
             }
             size_t index = row_A * matrix_to_multiply.cols + row_B;
-            result_matrix->data[index] = current_element;
+            result_matrix.data[index] = current_element;
         }
     }
-    return *result_matrix;
+    return result_matrix;
 }
 
 
-Matrix& Matrix::operator*(const double multiplier) {
-    std::cout << "вызван оператор умножения на скаляр" << std::endl;
+Matrix Matrix::operator*(const double multiplier) {
+    std::cout << "вызван оператор умножения на скаляр" << std::endl;  // для разработчика
 
-    Matrix *result_matrix = new Matrix(cols, rows);
+    Matrix result_matrix = Matrix(cols, rows);
 
     for(size_t index = 0; index < rows * cols; index++) {
-        result_matrix->data[index] = data[index] * multiplier;
+        result_matrix.data[index] = data[index] * multiplier;
     }
-    return *result_matrix;
+    return result_matrix;
 }
 
 
-Matrix& Matrix::exponential(unsigned int total_iterations) 
+Matrix Matrix::exponential(unsigned int total_iterations) 
 {
-    std::cout << "вызвана операция экспененты" << std::endl;
+    std::cout << "вызвана операция экспененты" << std::endl;  // для разработчика
     // проверка что матрица квадратная
     if(cols != rows) {
-        throw std::invalid_argument("Для выполнения операции возведения в степень матрица должна быть квадратной");
+        throw matrix_size_error("Для выполнения операции возведения в степень матрица должна быть квадратной");
     }
     
-    Matrix *result_matrix = new Matrix(cols, rows);
-    result_matrix->unit();  // результирующая матрица инициализируется как единичная
-    Matrix *prev_iteration_matrix = new Matrix(cols, rows);  // матрица для хранения предыдущего слагаемого
-    prev_iteration_matrix->unit();
+    Matrix result_matrix = Matrix(cols, rows);
+    result_matrix.unit();  // результирующая матрица инициализируется как единичная
+    Matrix prev_iteration_matrix = Matrix(cols, rows);  // матрица для хранения предыдущего слагаемого
+    prev_iteration_matrix.unit();
 
     for(unsigned int iteration = 1; iteration < total_iterations; iteration++) {
-        Matrix intermediate_matrix = *prev_iteration_matrix * *this;
-        std::cout << intermediate_matrix << std::endl;
-        *prev_iteration_matrix = intermediate_matrix * (1/(float)iteration);
-        *result_matrix = *result_matrix + *prev_iteration_matrix;
+        Matrix intermediate_matrix = prev_iteration_matrix * *this;
+        prev_iteration_matrix = intermediate_matrix * (1/(float)iteration);
+        result_matrix = result_matrix + prev_iteration_matrix;
     }
-    return *result_matrix;
+    return result_matrix;
+}
+
+
+double Matrix::det(double accuracy) 
+{
+    // проверка что матрица квадратная
+    if(cols != rows) {
+        throw matrix_size_error("Для вычисления определителя матрица должна быть квадратной");
+        return NAN;
+    }
+
+    if(data == NULL) {
+        throw matrix_data_error("Для вычисления определителя матрица не должна быть пустой");
+        return NAN;
+    }
+
+    Matrix matrix = Matrix(cols, rows);
+    size_t matrix_size = matrix.cols;
+
+    memcpy(matrix.data, data, sizeof(double) * cols * rows);
+
+    double det = 1.;
+
+    for(size_t col = 0; col < matrix_size - 1; col++) {  // преобразование матрицы к треугольнику
+        if(fabs(matrix.data[col * matrix_size + col]) < accuracy) {
+            for(size_t row_to_add = col + 1; row_to_add < matrix_size; row_to_add++) {
+                if(fabs(matrix.data[row_to_add * matrix_size + col])  > accuracy) {
+                    matrix.matrix_row_add(col, row_to_add, 1);
+                    break;
+                }
+                if(row_to_add == matrix_size - 1) {  // если в столбце все элементы равны 0, то определитель равен 0
+                    return 0;
+                }
+            }
+        }
+        for(size_t row = col + 1; row < matrix_size; row++) {
+            double multiplyer = matrix.data[row * matrix_size + col] / matrix.data[col * matrix_size + col];
+            matrix.matrix_row_substract(row, col, multiplyer);
+        }
+    }
+
+    for(size_t index = 0; index < matrix.rows; index++) {  // вычисление определителя
+        det *= matrix.data[index * (matrix.rows + 1)];
+    }
+
+    if(fabs(det) < accuracy) {
+        det = 0.0;
+    }
+
+    return det;
 }
 
 
@@ -259,12 +358,15 @@ int main()
     matrix_A.fill_random(10);
     matrix_B.fill_random(10);
 
-    Matrix matrix_C = matrix_A.exponential(30);
+    Matrix matrix_C = matrix_A + matrix_B;
+    //double det = matrix_A.det(0.00001);
     std::cout << std::endl;
 
     std::cout << matrix_A;
     std::cout << matrix_B;
     std::cout << matrix_C;
+    //std::cout << det;
+    //std::cout << std::endl;
 
     return 0;
 }
