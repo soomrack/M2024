8f54d256e633a54dfffcb41d3c2ea1594586d3df
Valentina <Ms.ValentinaRF@mail.ru>
2024-05-12 20:33:39 +0300

    Fixed bugs

--- blokhina_va/task2/main.c
+++ blokhina_va/task2/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
 typedef struct {
     size_t rows;
@@ -35,16 +36,16 @@ void handle_error(enum Error error) {
             printf("Matrix size.\n");
             break;
         case FIRST_MATCOLS_IS_NOT_EQUAL_SECOND_MATROWS:
-            printf("Количество стобцов первой матрицы не равно количеству строк второй матрицы.\n");
+            printf("The number of columns of the first matrix is not equal to the number of rows of the second matrix.\n");
             break;
         case TOO_LARGE_MATRIX_SIZE:
-            printf("Матрица больше, чем ожидалось.\n");
+            printf("The matrix is larger than expected.\n");
             break;
         case MATRIX_IS_NOT_SQUARE:
-            printf("Матрица не квадратная.\n");
+            printf("The matrix is not square.\n");
             break;
         case UNDENTIFY_ERROR:
-            printf("Неизвестная ошибка.\n");
+            printf("Unknown error.\n");
             break;
     }
 }
@@ -96,7 +97,7 @@ void matrix_filling(Matrix *mat) {
 }
 
 // Функция для вычисления факториала
-double factorial(int n) {
+double factorial(size_t n) {
     double result = 1.0;
     for (size_t i = 2; i <= n; ++i) {
         result *= i;
@@ -105,22 +106,20 @@ double factorial(int n) {
 }
 
 // Единичная матрица
-Matrix matrix_unit(size_t rows, size_t cols) {
-    Matrix matrixUnit = matrix_create(rows, cols);
-    if (matrixUnit.data == NULL) {
+Matrix matrix_identity(size_t rows, size_t cols) {
+    Matrix matrixIdentity = matrix_create(rows, cols);
+    if (matrixIdentity.data == NULL) {
         handle_error(MEMORY_ALLOCATION_ERROR);
         return MATRIX_NULL;
     }
-    for (size_t i = 0; i < rows; i++) {
-        for (size_t j = 0; j < cols; j++) {
-            if (i == j){
-                matrixUnit.data[i] = 1.0;
-            } 
-            else matrixUnit.data[i] = 0.0;
-        }
+    size_t mem_size = rows * cols * sizeof(double);
+    memset(matrixIdentity.data, 0, mem_size);
+
+    for (size_t index = 0; index < rows; index++){
+        matrixIdentity.data[index * rows + index] = 1.;
     }
 
-    return matrixUnit;
+    return matrixIdentity;
 }
 
 // Вывод матрицы на экран
@@ -144,11 +143,11 @@ void multiplication_by_number(Matrix *mat, double number){
 // Сложение матриц
 void summation(Matrix *frst, Matrix *scnd){
     if (frst->rows != scnd->rows || frst->cols != scnd->cols){
-        handle_error(3);
+        handle_error(MATRIX_SIZE_IS_NOT_EQUAL);
     }
     for (size_t i = 0; i < frst->rows; ++i) {
         for (size_t j = 0; j < frst->cols; ++j) {
-            frst->data[i * frst->cols + j] = \
+            frst->data[i * frst->cols + j] =
                 frst->data[i * frst->cols + j] + scnd->data[i * scnd->cols + j];
         }
     }
@@ -157,11 +156,11 @@ void summation(Matrix *frst, Matrix *scnd){
 // Вычитание матриц
 void subtraction(Matrix *frst, Matrix *scnd){
     if (frst->rows != scnd->rows || frst->cols != scnd->cols){
-        handle_error(3);
+        handle_error(MATRIX_SIZE_IS_NOT_EQUAL);
     }
-    for (int i = 0; i < frst->rows; ++i) {
-        for (int j = 0; j < frst->cols; ++j) {
-            frst->data[i * frst->cols + j] = \
+    for (size_t i = 0; i < frst->rows; ++i) {
+        for (size_t j = 0; j < frst->cols; ++j) {
+            frst->data[i * frst->cols + j] =\
                 frst->data[i * frst->cols + j] - scnd->data[i * scnd->cols + j];
         }
     }
@@ -170,13 +169,13 @@ void subtraction(Matrix *frst, Matrix *scnd){
 // Умножение матриц
 void multiplication(Matrix *frst, Matrix *scnd){
     if (frst->cols != scnd->rows){
-        handle_error(4);
+        handle_error(FIRST_MATCOLS_IS_NOT_EQUAL_SECOND_MATROWS);
     }
-    for (int frst_rows = 0; frst_rows < frst->rows; frst_rows++) {
-        for (int scnd_mat_cols = 0; scnd_mat_cols < scnd->cols; scnd_mat_cols++) {
+    for (size_t frst_rows = 0; frst_rows < frst->rows; frst_rows++) {
+        for (size_t scnd_mat_cols = 0; scnd_mat_cols < scnd->cols; scnd_mat_cols++) {
             double sum = 0;
-            for (int elmnts = 0; elmnts < frst->rows; ++elmnts) {
-                sum += frst->data[frst_rows * frst->cols + elmnts] * \
+            for (size_t elmnts = 0; elmnts < frst->cols; ++elmnts) {
+                sum += frst->data[frst_rows * frst->cols + elmnts] *\
                     scnd->data[elmnts * scnd->cols + scnd_mat_cols];
             }
         }
@@ -190,22 +189,24 @@ Matrix matrix_exponential(Matrix *mat) {
         return MATRIX_NULL;
     }
     // Начальное значение единичная матрица
-    Matrix result = matrix_unit(mat->rows, mat->cols);
+    Matrix result = matrix_identity(mat->rows, mat->cols);
+    if (result.data == NULL) {
+        handle_error(MEMORY_ALLOCATION_ERROR);
+        return MATRIX_NULL;
+    }
     // Вспомогательная матрица
-    Matrix temp = matrix_create(mat->rows, mat->cols);
+    Matrix temp = matrix_identity(mat->rows, mat->cols);
+    if (temp.data == NULL) {
+        handle_error(MEMORY_ALLOCATION_ERROR);
+        matrix_free(&result);
+        return MATRIX_NULL;
+    }
 
-    for (size_t k = 1; k <= 10; ++k) {
-        double factor = 1.0 / factorial(k);
-
-        // Добавляем факториал * на текущий член к текущему члену ряда
-        for (size_t i = 0; i < result.rows; i++) {
-            for (size_t j = 0; j < result.cols; j++) {
-                result.data[i * result.cols + j] += factor * temp.data[i * temp.cols + j];
-            }
-        }
-
-        // Умножаем матрицу-слагаемое на исходную матрицу
+    // Расчёт экспоненциального ряда
+    for (size_t k = 1; k <= 10; k++) {
         multiplication(&temp, mat);
+        multiplication_by_number(&temp, 1. / k);
+        summation(&result, &temp);
     }
 
     matrix_free(&temp);
@@ -217,24 +218,24 @@ double determinant(Matrix *mat){
     double det;
     if (mat->rows != mat->cols){
         handle_error(MATRIX_IS_NOT_SQUARE);
-        return nan("");
+        return NAN;
     }
     if (mat->rows == 1)
         det = mat->data[0];
     else if (mat->rows == 2)
         det = mat->data[0] * mat->data[3] - mat->data[1] * mat->data[2];
     else if (mat->rows == 3){
-        det = \
-        mat->data[0] * mat->data[4] * mat->data[8] +\
-        mat->data[1] * mat->data[2] * mat->data[5] +\
-        mat->data[3] * mat->data[7] * mat->data[6] -\
-        mat->data[6] * mat->data[4] * mat->data[2] -\
-        mat->data[7] * mat->data[5] * mat->data[8] -\
+        det =
+        mat->data[0] * mat->data[4] * mat->data[8] +
+        mat->data[1] * mat->data[2] * mat->data[5] +
+        mat->data[3] * mat->data[7] * mat->data[6] -
+        mat->data[6] * mat->data[4] * mat->data[2] -
+        mat->data[7] * mat->data[5] * mat->data[8] -
         mat->data[3] * mat->data[1] * mat->data[0];
     }
     else{
         handle_error(TOO_LARGE_MATRIX_SIZE);
-        return nan("");
+        return NAN;
     }
 
     return det;
