f4b08fbbd4042d9fa3a1f1368866c9e729df52fa
Maksim <61917118+Makozar@users.noreply.github.com>
2024-05-31 10:50:50 +0300

    Update Task2.c

--- Konstantinov/Task2.c
+++ Konstantinov/Task2.c
@@ -218,12 +218,13 @@ struct Matrix sum_for_e(const size_t deg_acc, const struct Matrix A){
         return E;
     }
 
+    E = A;
+
     if (deg_acc == 2) {
-        return A;
+        return E;
     }
 
     if (deg_acc > 2) {
-        E = A;
         for (size_t id = 2; id < deg_acc; ++id) {
             struct Matrix buf = E;
             E = matrix_mult(buf, A);
